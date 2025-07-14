a24186399b9e102b0b33b37f6b3f8d2c43623dd7
ghalaotb1 <gageotb12@gmail.com>
2025-07-14 22:13:19 +0300

    Update read_csv.py with data analysis code

--- read_csv.py
+++ read_csv.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('countries.csv')
-print(df.head())
+print('كل البيانات:')
+print(df)
+print('عدد الدول:', len(df))
+print('إجمالي عدد السكان:', df['population'].sum())
+print('متوسط عدد السكان:', df['population'].mean())
+print('أكبر دولة سكاناً:', df.loc[df['population'].idxmax(), 'country'])
 df.plot(kind='bar', x='country', y='population')
+plt.title('عدد السكان لكل دولة')
+plt.xlabel('الدولة')
+plt.ylabel('عدد السكان')
 plt.show()
